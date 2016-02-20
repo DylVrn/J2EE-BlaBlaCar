8ce31004dc9f4f20285b23a763b2eef97ffea65a
DylVrn <compte0inutile@gmail.com>
2016-02-20 17:14:03 +0100

    Added files via upload

---
+++ BlaBlaCar-ejb/src/java/service/UtilisateurService.java
@@ -0,0 +1,101 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package service;
+
+import java.util.List;
+import javax.ejb.Local;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.PersistenceUnit;
+import javax.persistence.Query;
+import metier.interfaces.UtilisateurInterface;
+import pojos.Avis;
+import pojos.Trajet;
+import pojos.Utilisateur;
+
+/**
+ *
+ * @author dyvernin
+ */
+@Stateless
+@Local(UtilisateurInterface.class)
+public class UtilisateurService implements UtilisateurInterface {
+
+    @PersistenceUnit(unitName = "BlaBlaCar-ejbPU")
+    private EntityManagerFactory emf = Persistence.createEntityManagerFactory("BlaBlaCar-ejbPU");
+
+    public UtilisateurService() {
+        //this.emf = Persistence.createEntityManagerFactory("toto");
+        //this.em = emf.createEntityManager();
+    }
+
+    @Override
+    public void inscrireTrajet(Trajet trajet) {
+
+    }
+
+    @Override
+    public void deposeTrajet(Utilisateur utilisateur, Trajet trajet) {
+        if (trajet != null) {
+            EntityManager em = this.emf.createEntityManager();
+            String nomUtilisateur = utilisateur.getNom();
+            String prenomUtilisateur = utilisateur.getPrenom();
+            Query query = em.createQuery("FROM Utilisateur WHERE prenom=:prenom AND nom=:nom");
+            query.setParameter("prenom", prenomUtilisateur);
+            query.setParameter("nom", nomUtilisateur);
+            // Obtention de la liste des utilisateurs portant ce nom et ce prenom : normalement un seul
+            List<Utilisateur> listeUtilisateurs = query.getResultList();
+
+            System.out.println("HHHHHHHHHHHHHHHHHHH -- utilisateur trouvé -- HHHHHHHHHHHHHHHHHHH");
+
+            if (listeUtilisateurs.isEmpty()) { // s'il n'y a aucun utilisateur avec ce nom et prenom dans la base, on en créé un nouveau
+                trajet.setUtilisateur_conducteur(utilisateur);
+                utilisateur.getTrajets().add(trajet);
+                System.err.println(utilisateur.getNom());
+                em.persist(utilisateur);
+            } else {
+                final Utilisateur userTmp = listeUtilisateurs.get(0);
+                trajet.setUtilisateur_conducteur(userTmp); // sinon on associe le premier trouvé au nouveau trajet
+                userTmp.getTrajets().add(trajet);
+            }
+            if (!em.getTransaction().isActive()) {
+                em.getTransaction().begin();
+            }
+            em.persist(trajet);
+            em.getTransaction().commit();
+            em.close();
+        }
+    }
+
+    @Override
+    public void deposeAvis(Utilisateur utilisateur, Avis avis) {
+
+    }
+
+    @Override
+    public void acceptePassagers(List<Utilisateur> listePassagers) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void paye(Trajet trajet) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void createUser(Utilisateur user) {
+        EntityManager em = this.emf.createEntityManager();
+
+        em.getTransaction().begin();
+        em.persist(user);
+        em.getTransaction().commit();
+
+        System.out.println("!!!! Utilisateur créé : createUser de UtilisateurService !!!!");
+    }
+
+}
